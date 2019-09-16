ccf66c0d1ced4f8f7ecdb233de9b3e1681a94675
Henry King <kingjoshking999@gmail.com>
2019-09-16 12:43:34 +0800

    To be deployed

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ document.querySelector("form").addEventListener("submit", e => {
 	const address = document.querySelector("input").value;
 
 	document.querySelector("#message-1").textContent = "";
-	document.querySelector("#message-2").textContent = "";
+	document.querySelector("#message-2").textContent = " ";
 
 	fetch(`http://localhost:3000/weather?address=${address}`)
 		.then(res => res.json())
