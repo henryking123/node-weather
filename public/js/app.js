8f91e8bf40e7aba7bf0d90569a6e62339a8a4a05
Henry King <kingjoshking999@gmail.com>
2019-09-16 16:39:29 +0800

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ document.querySelector("form").addEventListener("submit", e => {
 	document.querySelector("#message-1").textContent = "";
 	document.querySelector("#message-2").textContent = " ";
 
-	fetch(`http://localhost:3000/weather?address=${address}`)
+	fetch(`/weather?address=${address}`)
 		.then(res => res.json())
 		.then(data => {
 			if (data.error) return (document.querySelector("#message-1").textContent = data.error);
